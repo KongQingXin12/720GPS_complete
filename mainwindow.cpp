98ae7484ca3d1ee826ab5b657848c421a5a27137
KongQingXin12 <john920817@outlook.com>
2018-10-02 21:04:57 +0800

    1.0

---
+++ mainwindow.cpp
@@ -0,0 +1,50 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "readtxt.h"
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_OpenSourceFile_clicked()
+{
+    filename=QFileDialog::getOpenFileName(this,QString::fromLocal8Bit("源文件"),"",tr("text(*.txt)"));
+    ui->dis_mainwindow->append(QString::fromLocal8Bit("打开源文件"));
+    ui->dis_mainwindow->append(QString::fromLocal8Bit("源文件路径：")+filename);
+}
+
+void MainWindow::on_inputfilepath_clicked()
+{
+    resultFilename=QFileDialog::getExistingDirectory(this,QString::fromLocal8Bit("保存"),"/home",QFileDialog::ShowDirsOnly|QFileDialog::DontResolveSymlinks);
+    QString temp=resultFilename+QString("/gps_pos_834");
+    gps_pos=temp.toStdString();
+    temp=resultFilename+QString("/mode");
+    gps_xml=temp.toStdString();
+    gps=resultFilename.toStdString();
+    ui->dis_mainwindow->append(QString::fromLocal8Bit("路径打开成功"));
+}
+
+void MainWindow::on_deal_with_clicked()
+{
+    //提取文件内容
+    ui->dis_mainwindow->append(QString::fromLocal8Bit("开始处理"));
+    readtxt s(source,gps_pos);
+    s.trans();
+    GPStxt_834 s1(gps_pos, gps,gps_xml);
+    dir = s1.deal_pos();
+    if(dir.size()!=0)
+        ui->dis_mainwindow->append(QString::fromLocal8Bit("处理完成"));
+    else
+        ui->dis_mainwindow->append(QString::fromLocal8Bit("处理失败"));
+
+}
---
+++ mainwindow.h
@@ -0,0 +1,47 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+#include <QMessageBox>
+#include <QDebug>
+#include <QFileDialog>
+#include <QFileInfo>
+#include "GPStxt_834.h"
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_OpenSourceFile_clicked();
+
+    void on_inputfilepath_clicked();
+
+    void on_deal_with_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QString filename;//文件名
+    vector<string> dir;//处理数组输出
+    QString resultFilename;//输出文件路径
+//    QString Name;
+//    QString Value;
+
+
+    string source;
+    string gps_pos;
+    string gps_xml;
+    string gps;
+};
+
+#endif // MAINWINDOW_H
